3a0267a5d97018b30ebb66b8a6e7f712cdc1c02d
KittiArm <kittipong.chan09@gmail.com>
2026-02-09 12:17:44 +0700

    Update record amounts for กิตติพงษ์ จัน.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ const data = [
     date: "1 พฤศจิกายน 2568",
     total: 5000,
     records: [
-      { name: "กิตติพงษ์ จัน.", amount: 5000, status: "โอนแล้ว" },
+      { name: "กิตติพงษ์ จัน.", amount: 4500, status: "โอนแล้ว" },
+      { name: "กิตติพงษ์ จัน.", amount: 500, status: "โอนแล้ว" },
     ],
   },
   {
