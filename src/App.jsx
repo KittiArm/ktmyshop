58c8d37d590aad2180cfb310e085163df653302e
KittiArm <kittipong.chan09@gmail.com>
2026-02-16 15:52:09 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -385,7 +385,7 @@ export default function App() {
       </div>
 
       
-      <div className="flex p-4 mb-4 text-sm text-yellow-500 rounded-base bg-yellow-200 border border-yellow-500" role="alert">
+      <div className="flex p-4 mb-4 text-sm text-yellow-500 rounded-md bg-yellow-100 border border-yellow-500" role="alert">
         <svg className="w-4 h-4 me-2 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/></svg>
         <span className="sr-only">Success</span>
         <div>
